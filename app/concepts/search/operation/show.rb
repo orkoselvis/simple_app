3e8eb9bc7adb7cc327a39024ef291e2aafffc168
Orko Elvis <ivanviter@i.ua>
2019-12-10 16:22:36 +0200

    Fixed the majority of rubocop errors

--- app/concepts/search/operation/show.rb
+++ app/concepts/search/operation/show.rb
@@ -13,27 +13,29 @@ module Search
       ITEM_NOT_FOUND = 'Not found!'.freeze
       JSON_FILE = JSON.parse(File.read('data.json'))
 
-      def main_search(ctx, _options)         #main search logic implemented in this method
+      def main_search(ctx, _options)
         search_word = ctx[:params].downcase
-        ctx[:words] = []
+        ctx[:words] = [] # main search logic implemented in this method
         JSON_FILE.each do |line|
           ctx[:words] << line if line.values.map(&:downcase).grep(Regexp.new(search_word)).any? ||
                                  line.values.map(&:downcase).grep(Regexp.new(reverse_string(search_word))).any?
         end
       end
 
-      def reverse_string(word)               #this method transforms to reverse string (lisp common == common lisp)
-        word.split.reverse.join(' ')
+      def reverse_string(word)
+        word.split.reverse.join(' ') # this method transforms to reverse string (lisp common == common lisp)
       end
 
-      def check_amount(ctx, _options)       #this method checks if search word include only 1 word and returns in next method (type_n_designed) to avoid errors 
+      def check_amount(ctx, _options)
         return if ctx[:params].split.length == 1
 
+        # this method checks if search word include only 1 word and returns in next method (type_n_designed)
+        # to avoid errors
         true
       end
 
-      def type_n_designed(ctx, _options)   #this method finds result that matchs in different fields
-        search_word = ctx[:params].downcase
+      def type_n_designed(ctx, _options)
+        search_word = ctx[:params].downcase # this method finds result that matchs in different fields
         ctx[:words] = []
         JSON_FILE.each do |i|
           ctx[:words] << i if search_word.split.any? { |w| i['Type'].downcase.split(', ').include? w } &&
@@ -43,12 +45,12 @@ module Search
         end
       end
 
-      def negative_word?(ctx, _options)   #this method checks if search word include negative symbol
-        ctx[:params].split.last.include?('-')
+      def negative_word?(ctx, _options)
+        ctx[:params].split.last.include?('-') # this method checks if search word include negative symbol
       end
 
-      def negative_search(ctx, _options)  #this method finds negative results that matchs in different fields
-        search_word = ctx[:params].downcase
+      def negative_search(ctx, _options)
+        search_word = ctx[:params].downcase # this method finds negative results that matchs in different fields
         ctx[:words] = []
         new_words = search_word.tr('-', '').split
         JSON_FILE.each do |i|
@@ -58,7 +60,7 @@ module Search
       end
 
       def show_error(_options, ctx)
-        ctx[:words] << ITEM_NOT_FOUND     #this method returns 'not found' if search result is empty
+        ctx[:words] << ITEM_NOT_FOUND # this method returns 'not found' if search result is empty
       end
     end
   end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     search_word = params[:text]
     if search_word.present?
-      result = Search::Operation::Show.(params: search_word)
+      result = Search::Operation::Show.call(params: search_word)
       render html: cell(Search::Cell::Show, result)
     else
       render html: cell(Search::Cell::Index, result)
